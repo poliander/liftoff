2efd2c7223202ba2f34d565bb0fd9cab1b276d0a
poliander <poliander@mailbox.org>
2019-06-24 18:56:05 +0200

    Renamed mesh back to object (since its basically an .obj loader)

---
+++ src/object.cpp
@@ -0,0 +1,214 @@
+#include "object.hpp"
+
+Object::Object(string filename)
+{
+    if (load(filename)) {
+        index();
+    }
+}
+
+Object::~Object()
+{
+}
+
+bool Object::load(string filename)
+{
+    ifstream file;
+    string line;
+
+    file.open(filename.c_str());
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    while (file.good()) {
+        getline(file, line);
+
+        if (line.length() < 2) {
+            continue;
+        }
+
+        switch (line.at(0)) {
+            case 'v':
+                switch (line.at(1)) {
+                    case 't':
+                        objTexCoords.push_back(toVec2(line));
+                        break;
+
+                    case 'n':
+                        objNormals.push_back(toVec3(line));
+                        break;
+
+                    case ' ':
+                        objVertices.push_back(toVec3(line));
+                        break;
+                }
+                break;
+
+            case 'f':
+                vector<string> tokens = split(line, ' ');
+
+                objIndices.push_back(toIndex(tokens[1]));
+                objIndices.push_back(toIndex(tokens[2]));
+                objIndices.push_back(toIndex(tokens[3]));
+                break;
+        }
+    }
+
+    return true;
+}
+
+void Object::index()
+{
+    std::vector<index_t*> lookup;
+    
+    for (unsigned int i = 0; i < objIndices.size(); i++) {
+        lookup.push_back(&objIndices[i]);
+    }
+    
+    sort(lookup.begin(), lookup.end(), Object::compareIndex);
+
+    positions.clear();
+    texCoords.clear();
+    normals.clear();
+    indices.clear();
+
+    for (unsigned int i = 0; i < objIndices.size(); i++) {
+        index_t* currentIndex = &objIndices[i];
+
+        unsigned int vertexIndex = previousVertexIndex(lookup, currentIndex);
+
+        if (vertexIndex == -1) {
+            vertexIndex = positions.size();
+
+            positions.push_back(objVertices[currentIndex->vertex]);
+            texCoords.push_back(objTexCoords[currentIndex->uv]);
+            normals.push_back(objNormals[currentIndex->normal]);
+        }
+
+        indices.push_back(vertexIndex);
+    }
+}
+
+unsigned int Object::previousVertexIndex(vector<index_t*> &lookup, index_t* currentIndex)
+{
+    unsigned int start = 0;
+    unsigned int end = lookup.size();
+    unsigned int current = (end - start) / 2 + start;
+    unsigned int previous = start;
+    
+    while (current != previous) {
+        index_t* testIndex = lookup[current];
+        
+        if (testIndex->vertex == currentIndex->vertex) {
+            unsigned int countStart = current;
+        
+            for (unsigned int i = 0; i < current; i++) {
+                index_t* possibleIndex = lookup[current - i];
+                
+                if (possibleIndex == currentIndex) {
+                    continue;
+                }
+                    
+                if (possibleIndex->vertex != currentIndex->vertex) {
+                    break;
+                }
+                    
+                countStart--;
+            }
+            
+            for (unsigned int i = countStart; i < lookup.size() - countStart; i++) {
+                index_t* possibleIndex = lookup[current + i];
+                
+                if (possibleIndex == currentIndex) {
+                    continue;
+                }
+                    
+                if (possibleIndex->vertex != currentIndex->vertex) {
+                    break;
+                } else if (
+                    (possibleIndex->uv == currentIndex->uv) &&
+                    (possibleIndex->normal == currentIndex->normal)
+                ) {
+                    glm::vec3 currentPosition = objVertices[currentIndex->vertex];
+                    glm::vec2 currentTexCoord = objTexCoords[currentIndex->uv];
+                    glm::vec3 currentNormal = objNormals[currentIndex->normal];
+                    
+                    for (unsigned int j = 0; j < positions.size(); j++) {
+                        if (
+                            currentPosition == positions[j] &&
+                            currentTexCoord == texCoords[j] &&
+                            currentNormal == normals[j]
+                        ) {
+                            return j;
+                        }
+                    }
+                }
+            }
+        
+            return -1;
+        } else {
+            if (testIndex->vertex < currentIndex->vertex) {
+                start = current;
+            } else {
+                end = current;
+            }
+        }
+    
+        previous = current;
+        current = (end - start) / 2 + start;
+    }
+    
+    return -1;
+}
+
+glm::vec2 Object::toVec2(const string& line)
+{
+    float x, y;
+
+    sscanf(line.c_str(), "%*s %f %f", &x, &y);
+
+    return glm::vec2(x, y);
+}
+
+glm::vec3 Object::toVec3(const string& line)
+{
+    float x, y, z;
+
+    sscanf(line.c_str(), "%*s %f %f %f", &x, &y, &z);
+
+    return glm::vec3(x, y, z);
+}
+
+index_t Object::toIndex(const string& line)
+{
+    unsigned int v, u, n;
+
+    sscanf(line.c_str(), "%d/%d/%d", &v, &u, &n);
+
+    return index_t { --v, --u, --n };
+}
+
+vector<string> Object::split(string line, char delim)
+{
+    vector<string> result;
+
+    while (line.size()) {
+        int i = line.find(delim);
+
+        if (i != string::npos) {
+            result.push_back(line.substr(0, i));
+            line = line.substr(i + 1);
+
+            if (line.size() == 0) {
+                result.push_back(line);
+            }
+        } else {
+            result.push_back(line);
+            line = "";
+        }
+    }
+
+    return result;
+}
---
+++ src/object.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+using namespace std;
+
+struct index_t
+{
+    unsigned int vertex;
+    unsigned int uv;
+    unsigned int normal;
+
+    bool operator < (const index_t& r) const { return vertex < r.vertex; }
+};
+
+class Object
+{
+    public:
+        Object(string filename);
+        ~Object();
+
+        vector<glm::vec3>    positions;
+        vector<glm::vec2>    texCoords;
+        vector<glm::vec3>    normals;
+        vector<unsigned int> indices;
+
+        static bool compareIndex(const index_t* a, const index_t* b) { return a->vertex < b->vertex; }
+
+    private:
+        vector<glm::vec3>    objVertices;
+        vector<glm::vec2>    objTexCoords;
+        vector<glm::vec3>    objNormals;
+        vector<index_t>      objIndices;
+
+        bool load(string filename);
+        void index();
+        unsigned int previousVertexIndex(vector<index_t*> &lookup, index_t* currentIndex);
+
+        vector<string> split(string txt, char delim);
+        index_t toIndex(const string& token);
+
+        glm::vec2 toVec2(const string& line);
+        glm::vec3 toVec3(const string& line);
+};
