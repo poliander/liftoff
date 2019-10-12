29de3a4ef8ca240f3ec710f0dfb94123184dd8db
poliander <poliander@mailbox.org>
2019-10-12 19:04:50 +0200

    Larger framebuffer size

--- src/texture.cpp
+++ src/texture.cpp
@@ -6,7 +6,7 @@ Texture::Texture()
 
     init();
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1024, 1024, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2048, 2048, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -193,7 +193,7 @@ void Texture::bindFrameBuffer()
     if (frameBuffer != -1) {
         glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
         glGetIntegerv(GL_VIEWPORT, viewport);
-        glViewport(0, 0, 1024, 1024);
+        glViewport(0, 0, 2048, 2048);
     }
 }
 
