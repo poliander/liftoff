fe7fd1501281c0bb1184626666dd6e72345df471
René Pollesch <poliander@mailbox.org>
2024-02-27 14:06:49 +0100

    Always perform autoreconf before build

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 set -e
 
 if [ -z "$1" ]; then
-    ./configure --prefix=/usr/x86_64-w64-mingw32 --host=x86_64-w64-mingw32 && \
+    autoreconf -f -i && ./configure --prefix=/usr/x86_64-w64-mingw32 --host=x86_64-w64-mingw32 && \
         make clean && \
         make && \
         ${STRIP} liftoff.exe && \
