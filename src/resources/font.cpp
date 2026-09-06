89503ba3a1be323ea5136e32ad1b7c1f6ce9d241
René Pollesch <poliander@mailbox.org>
2026-09-06 13:02:21 +0200

    Fixed a small font rendering glitch

--- src/resources/font.cpp
+++ src/resources/font.cpp
@@ -54,8 +54,8 @@ Font::Font(const string& filename, shared_ptr<Shader> s, uint8_t q) : Quad() {
             face->glyph->bitmap.buffer
         );
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
